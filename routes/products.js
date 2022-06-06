75e95fea76391597b5e8e3d5e3972f2ca4abd0e3
hasnat075 <98056763+hasnat075@users.noreply.github.com>
2022-06-06 23:11:25 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,10 @@
 var express = require("express");
-const { append } = require("express/lib/response");
 var router = express.Router();
 const Products = require("../models/products");
 /* GET users listing. */
 router.get("/", async (req, res, next) => {
   products = await Products.find();
-  res.render("products", products);
+  res.send(products);
 });
 router.get("/add", async (req, res, next) => {
   res.render("add_products");
@@ -20,7 +19,7 @@ router.post("/", async (req, res, next) => {
     body = req.body;
     var product = new Products(body);
     product.save();
-    res.redirect("/products");
+    res.send(product);
   } catch (err) {
     console.log(err.message);
     res.send({ status: "Error", message: "Type Mismatch" });
@@ -32,7 +31,8 @@ router.get("/delete/:id", async (req, res, next) => {
     console.log(id);
     if (id) {
       await Products.findByIdAndDelete(id);
-      res.redirect("/products");
+      // res.redirect("/products");
+      res.sendStatus(200);
     } else {
       throw new Error();
     }
@@ -41,19 +41,20 @@ router.get("/delete/:id", async (req, res, next) => {
     res.send({ status: "Error", message: "Product not Found" });
   }
 });
-router.post("/edit", async (req, res, next) => {
+router.post("/edit", async (req, res) => {
   body = req.body;
-  console.log(body);
   product = await Products.findById(body.id);
+
   product.name = body.name;
   product.price = body.price;
   product.type = body.type;
+  console.log(product);
   await product.save();
-  res.redirect("/products");
+  return res.send("OK");
 });
 router.get("/details/:id", async (req, res) => {
   const id = req.params.id;
   const product = await Products.findById(id);
-  res.render("details", product);
+  res.send(product);
 });
 module.exports = router;
